8ac1332ada36683a1b2ad5969ca4ffad815054cd
echizenyayota <virtualmisso30@gmail.com>
2018-03-01 15:48:20 +0900

    ルーティングの確認とエラー処理

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// ドキュメントルートディレクトリをオブジェクトで定義する
+var index = require('./routes/index');
+
 // 変数appにexpress()を代入するとappがオブジェクト化する
 var app = express();
 
@@ -20,6 +23,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.jsが読み込まれた後にアクセスするファイル
+app.use('/', index);
+
+// エラー処理
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// エラーの表示
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // ブラウザ上で表示が成功したことについてコマンドプロンプト上でメッセージを表示する
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -24,3 +24,5 @@ router.get('/', function(req, res, next) {
   });
 
 });
+
+module.exports = router;
