b6671b0d69a045faa028ea76ffe8e0f316f2572d
echizenyayota <virtualmisso30@gmail.com>
2018-03-01 13:35:57 +0900

    モジュールのインストールとミドルウェアの実装

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // expressモジュールの読み込み
 var express = require('express');
+var module-name = require('path');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 // 変数appにexpress()を代入するとappがオブジェクト化する
 var app = express();
 
@@ -8,6 +13,16 @@ app.get('/', function (req, res) {
   res.send('Hello World from Braintree!');
 });
 
+// モジュールの使用
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // ブラウザ上で表示が成功したことについてコマンドプロンプト上でメッセージを表示する
 app.listen(3000);
 console.log("server starting...");
+
+module.exports = app;
