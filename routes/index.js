004ced227e44a5141c6a28c78f8d276baeed20c3
echizenyayota <virtualmisso30@gmail.com>
2018-03-07 12:18:10 +0900

    index.jsにpostメソッド（/checkout）の追加

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,35 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.post("/checkout", function (req, res) {
+  // Use payment method nonce here
+  var gateway = braintree.connect({
+    accessToken: ACCESS_TOKEN
+　});
+  var saleRequest = {
+    amount: req.body.amount,
+    merchantAccountId: "JPY",
+    paymentMethodNonce: "" + req.body.payment_method_nonce,
+    options: {
+      submitForSettlement: true,
+      paypal: {
+        customField: "販売者:ショップ１"
+      }
+    },
+    descriptor: {
+      name: "PAYPAL *12345678901234"
+    }
+  };
+  gateway.transaction.sale(saleRequest, function (err, result) {
+    if (err) {
+      res.send("<h1>Error:  " + err + "</h1><br/>" + JSON.stringify(result, null, 4).replace(/\n/g, "\n<br/>").replace(/ /g, " &nbsp;") + "<br/><br/><a href=\"/\">Try again</a>");
+    } else if (result.success) {
+      res.send("<h1>Success! Transaction ID: " + result.transaction.id + "</h1><br/>" + JSON.stringify(result, null, 4).replace(/\n/g, "\n<br/>").replace(/ /g, " &nbsp;") + "<br/><br/><a href=\"/\">Try again</a>");
+    } else {
+      res.send("<h1>Error:  " + result.message + "</h1><br/>" + JSON.stringify(result, null, 4).replace(/\n/g, "\n<br/>").replace(/ /g, " &nbsp;") + "<br/><br/><a href=\"/\">Try again</a>");
+    }
+  });
+});
+  
+
 module.exports = router;
